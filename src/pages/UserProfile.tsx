0a79c05377c3be6e52a8f735d45da2cbf22b0ab3
adsyam <yambaoadrianne@gmail.com>
2023-11-21 18:17:02 +0800

    user profile fix

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,21 +1,33 @@
 import { useEffect, useState } from "react"
 import { Outlet, useLocation, useParams } from "react-router-dom"
 import { CoverPhoto, ProfileDetails, ProfilePic } from "../components"
-import { useAuthContext } from "../contexts/AuthContext"
-import { useDBContext } from "../contexts/DBContext"
-import { useDataContext } from "../contexts/DataContext"
+import { AuthContextProps, useAuthContext } from "../contexts/AuthContext"
+import { DBContextProps, useDBContext } from "../contexts/DBContext"
+import { DataContextProps, useDataContext } from "../contexts/DataContext"
 import { useFetchChannelDetails } from "../hooks/videoHooks"
+
+interface Snippet {
+  thumbnails: {
+    high: {
+      url: string
+    }
+  }
+}
+
+interface ChannelDetail {
+  snippet?: Partial<Snippet>
+}
 
 export default function UserProfile() {
   const { id } = useParams()
   const location = useLocation().pathname
   const { channelDetail } = useFetchChannelDetails(id)
-  const { user } = useAuthContext()
-  const { sidebar } = useDataContext()
-  const { getUserData } = useDBContext()
+  const { user } = useAuthContext() as AuthContextProps
+  const { sidebar } = useDataContext() as DataContextProps
+  const { getUserData } = useDBContext() as DBContextProps
   const [loading, setLoading] = useState(true)
   const [isUser, setIsUser] = useState(true)
-  const [detail, setDetail] = useState({})
+  const [detail, setDetail] = useState<ChannelDetail>({})
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -24,14 +36,14 @@ export default function UserProfile() {
         setIsUser(false)
         setLoading(false)
       } else {
-        getUserData(id).then((res) => setDetail(res))
+        getUserData(id ?? '').then((res) => setDetail(res || {}))
         setIsUser(true)
         setLoading(false)
       }
     }, 1000)
 
     return () => clearTimeout(timeout)
-  }, [id, channelDetail, user, location])
+  }, [id, channelDetail, user, location, getUserData])
 
   return (
     <section
